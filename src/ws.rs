f03918087571fe2ee4cd97a5b8639e65d9ed7859
1garo <alevardai@hotmail.com>
2021-01-08 18:59:38 -0300

    feat: implement ws module

--- src/ws.rs
+++ src/ws.rs
@@ -11,4 +11,59 @@ pub struct TopicsRequest {
 
 // theoric reference https://blog.logrocket.com/how-to-build-a-websocket-server-with-rust/
 // following https://github.com/zupzup/warp-websockets-example repo
-// TODO: Implement the ws mod 
+// TODO: Implement the ws mod
+pub async fn client_connection(ws: WebSocket, id: String, clients: Clients, mut client: Client) {
+	let (client_ws_sender, mut client_ws_rcv) = ws.split();
+	let (client_sender, client_rcv) = mpsc::unbounded_channel();
+
+	tokio::task::spawn(client_rcv.forward(client_ws_sender).map(|result| {
+		if let Err(e) = result {
+			eprintln!("error sending websocket msg: {}", e);
+		}
+	}));
+
+	client.sender = Some(client_sender);
+	clients.write().await.insert(id.clone(), client);
+
+	println!("{} connected", id);
+
+	while let Some(result) = client_ws_rcv.next().await {
+		let msg = match result {
+			Ok(msg) => msg,
+			Err(e) => {
+				eprintln!("error receiving ws message from id: {} - {}", id.clone(), e);
+				break;
+			}
+		};
+		client_msg(&id, msg, &clients).await;
+	}
+	clients.write().await.remove(&id);
+	println!("{} disconnected", id);
+}
+
+async fn client_msg(id: &str, msg: Message, clients: &Clients) {
+	println!("received message from {} - {:?}", id, msg);
+	let message = match msg.to_str() {
+		Ok(v) => v,
+		Err(_) => return,
+	};
+
+	// TODO: removed this igual message = "ping\n" with trim or something like that
+	if message == "ping" || message == "ping\n" {
+		return;
+	}
+
+	let topics_req: TopicsRequest = match from_str(&message) {
+		Ok(v) => v,
+		Err(e) => {
+			eprintln!("error while parsing message to topics request: {}", e);
+			return;
+		}
+	};
+
+	let mut locked = clients.write().await;
+
+	if let Some(v) = locked.get_mut(id) {
+		v.topics = topics_req.topics;
+	}
+}
